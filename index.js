cfdaceb0d787d5385e03bd8dbd56b7c862181c13
i5ting <i5ting@126.com>
2016-12-13 22:45:08 +0800

    refact, please ignore

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+'use strict'
+
 module.exports = class StaticPlugin {
 
   static () {
@@ -8,6 +10,47 @@ module.exports = class StaticPlugin {
       }
     }
 
+    this.gzip()
+    this.etag()
+    this.serve()
+  }
+
+  serve () {
+    if (!this.opts.static) {
+      this.opts.static = {
+        path: this.opts.root + '/public',
+        opts: {} // https://github.com/koajs/static#options
+      }
+    }
+
     this.use(require('koa-static')(this.opts.static.path, this.opts.static.opts))
   }
+
+  etag () {
+    if (!this.opts.static.etag) {
+      this.opts.static.etag = {
+      }
+    }
+
+    var conditional = require('koa-conditional-get')
+    var etag = require('koa-etag')
+
+    this.use(conditional())
+    this.use(etag())
+  }
+
+  gzip () {
+    if (!this.opts.static.gzip) {
+      this.opts.static.gzip = {
+        filter: function (contentType) {
+          return /text/i.test(contentType)
+        },
+        threshold: 2048,
+        flush: require('zlib').Z_SYNC_FLUSH
+      }
+    }
+    var compress = require('koa-compress')
+    this.use(compress())
+  }
+
 }
